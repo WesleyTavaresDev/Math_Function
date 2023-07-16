755e4ebf2dca352c9daaffa2a837b1e1d574a9b6
WesleyTavaresDev <wesleyTavaresDev@outlook.com>
2023-07-16 17:58:01 -0300

    fn is_crescent_decrescent_constant refactored

--- src/affine_function.rs
+++ src/affine_function.rs
@@ -5,36 +5,24 @@ pub mod affine {
     }
 
     impl AffineCoefficient {
-        /*pub fn is_crescent(coefficients: &AffineCoefficient) -> bool {
-            if coefficients.angular > 0.0 {
-                return true;
-            }
-            return false;
-        }
-
-        pub fn is_decrescent(coefficients: &AffineCoefficient) -> bool {
-            if coefficients.angular < 0.0 {
-                return true;
-            }
-            return false;
-        }*/
-
         pub fn is_crescent_decrescent_constant(coefficients: &AffineCoefficient) {
-            if coefficients.angular > 0.0 {
-                println!("Your function is crescent");
-            } else if coefficients.angular < 0.0 {
-                println!("Your function is decrescent");
-            } else {
-                println!("Your function is constant");
+            match coefficients.angular {
+                angular if angular > 0.0 => println!("Your function is crescent"),
+                angular if angular < 0.0 => println!("Your function is decrescent "),
+                _ => println!("Your function is constant"),
             }
-        }
-
-        pub fn zero_of_function(coefficients: &AffineCoefficient) -> f32 {
-            (-coefficients.linear) / coefficients.angular
         }
 
         pub fn function_summary(coefficients: &AffineCoefficient) {
-            println!("The function f(x) = {}x + {} and intersects the x axis on ({}, 0) and the y axis on (0, {})", coefficients.angular, coefficients.linear, Self::zero_of_function(&coefficients), coefficients.linear);
+            println!("The function f(x) = {}x + {} and intersects the x axis on ({}, 0) and the y axis on (0, {})", 
+                coefficients.angular, 
+                coefficients.linear, 
+                Self::zero_of_function(&coefficients), 
+                coefficients.linear);
+        }
+
+        fn zero_of_function(coefficients: &AffineCoefficient) -> f32 {
+            (-coefficients.linear) / coefficients.angular
         }
     }
 }
